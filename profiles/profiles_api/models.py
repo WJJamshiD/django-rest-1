5416fde32cb2b3ed4ebae1e198ea7288b8823333
Jamshid Yakhshiev <wjjamshid@gmail.com>
2020-09-07 22:16:39 +0500

    added custom user for authentication

---
+++ profiles/profiles_api/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import AbsractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, first_name, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name)
+
+        user.ser_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, first_name, password):
+        user = self.create_user(email, first_name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using.self._db)
+
+        return user
+
+
+class UserProfile(AbsractBaseUser,PermissionsMixin):
+    email = models.EmailField(max_length=254, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['last_name']
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_short_name(self):
+        return self.first_name
+
+    def __str__(self):
+        return str(email)
+
+    
