38fc7cb0aa21eb15f15a5a2fca704aecb46ef266
Jamshid Yakhshiev <wjjamshid@gmail.com>
2020-09-08 18:09:27 +0500

    added login API

--- profiles/profiles_api/views.py
+++ profiles/profiles_api/views.py
@@ -2,7 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.filters import SearchFilter
+from rest_framework.settings import api_settings
 from rest_framework import status
 from profiles_api.serializers import HelloSerializer, UserProfileSerializer
 from profiles_api.models import UserProfile
@@ -90,3 +92,8 @@ class UserProfileViewSet(ModelViewSet):
     permission_classes = (UpdateOwnProfile,)
     filter_backends = (SearchFilter,)
     search_fields = ('first_name', 'last_name', 'email', )
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
